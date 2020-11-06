0355870fa8377c9a7710d52168fa3f6dc89e1111
wisestMe <wyhzest@gmail.com>
2020-11-06 04:31:35 +0100

    toggle modal display

--- master.js
+++ master.js
@@ -1,13 +1,16 @@
 let hamburgerIcon = document.querySelector('.fa-bars');
 let closeIcon = document.querySelector('.fa-cross');
+let modal = document.querySelector('#modal');
 
 
 hamburgerIcon.addEventListener('click', () => {
     closeIcon.style.display = 'block';
     hamburgerIcon.style.display = 'none';
+    modal.style.display = 'block';
 });
 
 closeIcon.addEventListener('click', () => {
     closeIcon.style.display = 'none';
     hamburgerIcon.style.display = 'block';
+    modal.style.display = 'none';
 });
